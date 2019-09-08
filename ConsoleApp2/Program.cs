a2a2e754c93461026c63f81adaea2e209442db3f
lznick.zhang <lznick.zhang@gmail.com>
2019-09-08 22:19:36 +1200

    Generic interface

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+/*泛型接口的例子
+ * 
+ */
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student<int> s = new Student<int>();
+            s.ID = 101;
+            s.Name = "nick";
+            Console.WriteLine(s.ID + " name is " + s.Name);
+        }
+    }
+
+    interface IUnique<TId>
+    {
+        TId ID { get; set; }
+    }
+
+    //如果一个类继承的是泛型接口，那么这个类依然是泛型类
+    class Student<TId> : IUnique<TId>
+    {
+        public TId ID { get; set; }
+        public string Name { get; set; }
+    }
+}
